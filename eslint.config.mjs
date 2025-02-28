97ed856a78356707d412d1b881d875b86676999b
n-takeshita <naokisyuu@gmail.com>
2025-02-28 20:38:24 +0900

    add: eslint import order rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "import/order": [
+        "error",
+        {
+          alphabetize: { order: "asc" },
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
