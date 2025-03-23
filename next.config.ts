573d1899a8e19c7b8b723749251c93fef952adeb
n-takeshita <naokisyuu@gmail.com>
2025-03-23 14:33:20 +0900

    add: 開発環境用にoutputFileTracingRootを設定

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
-// import path from "path";
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // outputFileTracingRoot: path.join(__dirname, "../"),
+
+  // 開発環境では開発中のcoreロジックをnpm linkして動作確認するため、outputFileTracingRootを設定している。
+  outputFileTracingRoot:
+    process.env.NODE_ENV === "development"
+      ? path.join(__dirname, "../")
+      : undefined,
+
   experimental: {
     viewTransition: true,
   },
